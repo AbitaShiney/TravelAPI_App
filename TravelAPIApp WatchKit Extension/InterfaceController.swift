47f8d090bd22152328994f58fa4ae1e0f38a852d
himauli_patel <himaulipatel2195@gmail.com>
2019-03-16 13:55:26 -0400

    TabelView added in Watch Interface

--- TravelAPIApp WatchKit Extension/InterfaceController.swift
+++ TravelAPIApp WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,12 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var FlightTable: WKInterfaceTable!
+    
+    @IBAction func BookFlightButton() {
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,6 +26,8 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        self.FlightTable.setNumberOfRows(1, withRowType: "flight_cell")
     }
     
     override func didDeactivate() {
